4e5d71f1a30386a7a6b399978877cdc02d78dd43
Alex <temper8@gmail.com>
2025-11-25 21:56:49 +0300

    Create note_03.py

---
+++ note_03.py
@@ -0,0 +1,64 @@
+import marimo
+
+__generated_with = "0.18.0"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    return mo, np, plt
+
+
+@app.cell
+def _(np):
+    def mandelbrot_vectorized(width, height, x_min, x_max, y_min, y_max, max_iter):
+        """
+        Векторизованная версия для более быстрого вычисления
+        """
+        # Создаем сетку комплексных чисел
+        x = np.linspace(x_min, x_max, width)
+        y = np.linspace(y_min, y_max, height)
+        c = x + 1j * y[:, None]
+    
+        # Инициализируем массив z
+        z = np.zeros_like(c)
+        mandelbrot_set = np.zeros(c.shape, dtype=int)
+    
+        # Векторизованное вычисление
+        for i in range(max_iter):
+            mask = np.abs(z) <= 2
+            z[mask] = z[mask]**2 + c[mask]
+            mandelbrot_set[mask] = i
+    
+        return mandelbrot_set
+    return (mandelbrot_vectorized,)
+
+
+@app.cell
+def _(mandelbrot_vectorized):
+    # Параметры для более детального изображения
+    width, height = 1000, 1000
+    x_min, x_max = -2.0, 0.5
+    y_min, y_max = -1.25, 1.25
+    max_iter = 50
+    mandelbrot_set = mandelbrot_vectorized(width, height, x_min, x_max, y_min, y_max, max_iter)
+    return mandelbrot_set, x_max, x_min, y_max, y_min
+
+
+@app.cell
+def _(mandelbrot_set, mo, plt, x_max, x_min, y_max, y_min):
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.subplots()
+    ax.imshow(mandelbrot_set, extent=[x_min, x_max, y_min, y_max], cmap='hot')
+    ax.set_title('Манделььброт. Цветовая схема: hot')
+    #ax.colorbar()
+    mo.as_html(ax)
+    return
+
+
+if __name__ == "__main__":
+    app.run()
